49b3e5a3821e921131f8fa930bb02b375edeaaff
EasonWuTPE <tvjs168@gmail.com>
2019-09-03 22:45:31 +0800

    Update the structure of the project

--- src/main/java/com/searchfood/SearchFoodBackend/SearchFoodApplication.java
+++ src/main/java/com/searchfood/SearchFoodBackend/SearchFoodApplication.java
@@ -1,13 +1,13 @@
-package com.searchfood.backenend;
+package com.searchfood.SearchFoodBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SearchFoodApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SearchFoodApplication.class, args);
 	}
 
 }
